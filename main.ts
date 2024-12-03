4c8fab6a50c09a362863a20d74abb54a135795b8
Rick van der Staaij <id@rick.nu>
2024-12-03 19:21:28 +0100

    Create output folder and test file

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-export function add(a: number, b: number): number {
-	return a + b;
+console.log('Running converter...');
+
+const enpass = JSON.parse(await Deno.readTextFile(`${Deno.cwd()}/export.json`));
+
+console.log(enpass);
+
+try {
+	await Deno.mkdir('./1password')
+
+	console.log('Created directory %c./1password %cto output all 1password templates to.', 'color: green', 'color: initial');
+} catch (error) {
+	if (!(error instanceof Deno.errors.AlreadyExists)) {
+		throw error;
+	}
+
+	console.log('Directory %c./1password %calready exists.', 'color: green', 'color: initial');
 }
 
-console.log("Add 2 + 3 =", add(2, 3));
+Deno.writeTextFile('./1password/test.json', JSON.stringify({
+	newFile: true,
+}, null, '\t'))
